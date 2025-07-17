a318df69dd614128365009aed4b75f3dabc6c73f
RTXCR7 <jain.anurag0203@gmail.com>
2025-07-17 18:43:55 +0530

    Add project files.

---
+++ BlazeCart/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BlazeCart.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}

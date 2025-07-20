45b699a1b216ad2c081a85f408876b136461f1c3
RTXCR7 <jain.anurag0203@gmail.com>
2025-07-20 15:47:03 +0530

    Added Roles to the Authentication Services of Admin and Customer

---
+++ BlazeCart/Utility/SD.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazeCart.Utility
+{
+    public static class SD // sd --> static data
+    {
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+    }
+}
